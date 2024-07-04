b29631ac1fe971d49bc6d4344058ff02d16f18cc
Nikolai Cauper <nikolai.cauper@gmail.com>
2024-07-03 22:01:21 -0500

    tercer deploy

--- src/main/java/com/aula/sproject/SprojectApplication.java
+++ src/main/java/com/aula/sproject/SprojectApplication.java
@@ -1,17 +1,25 @@
 package com.aula.sproject;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.wavefront.WavefrontProperties.Application;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.aula.sproject.repository")
 @EntityScan(basePackages = "com.aula.sproject.entity")
-public class SprojectApplication {
+public class SprojectApplication  extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SprojectApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
 }
